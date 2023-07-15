0086f9dca877034d513ad1e09e2b88f7abca3050
byulcode <bybit0408@gmail.com>
2023-07-15 17:58:04 +0900

    맞춤형 인증 논리 구현

--- src/main/java/study/springsecurity/config/ProjectConfig.java
+++ src/main/java/study/springsecurity/config/ProjectConfig.java
@@ -2,8 +2,10 @@ package study.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -15,6 +17,16 @@ public class ProjectConfig {
 
     @Bean
     public SCryptPasswordEncoder sCryptPasswordEncoder() {
-        return new SCryptPasswordEncoder(65536,8,1,32,16);
+        return new SCryptPasswordEncoder(65536, 8, 1, 32, 16);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated();
+        return http.build();
     }
 }
