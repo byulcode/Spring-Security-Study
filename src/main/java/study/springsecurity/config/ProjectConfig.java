948d589c6d938e6614e5394d88bf7d13e6f2b553
byulcode <bybit0408@gmail.com>
2023-07-17 18:38:12 +0900

    WRITE 권한이 있는 사용자만 접근 허용

--- src/main/java/study/springsecurity/config/ProjectConfig.java
+++ src/main/java/study/springsecurity/config/ProjectConfig.java
@@ -2,11 +2,13 @@ package study.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -34,5 +36,15 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        (authz) -> authz.anyRequest()
+                                .hasAnyAuthority("WRITE"))
+                .httpBasic();
+        return http.build();
     }
 }
