06c023fad1a8fea88fa0e0c8aea2b95b663b4d9b
byulcode <bybit0408@gmail.com>
2023-07-25 10:21:08 +0900

    양식을 통해 CSRF 토큰을 요청에 추가하는 실제 애플리케이션 구현

--- src/main/java/study/springsecurity10/config/ProjectConfig.java
+++ src/main/java/study/springsecurity10/config/ProjectConfig.java
@@ -3,19 +3,42 @@ package study.springsecurity10.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfFilter;
-import study.springsecurity10.filter.CsrfTokenLogger;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user1 = User.withUsername("mary")
+                .password("12345")
+                .authorities("READ")
+                .build();
+
+        userDetailsService.createUser(user1);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-                addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
+        http
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
 
         return http.build();
     }
